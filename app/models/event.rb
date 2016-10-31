d52320ad21a99bc444055c5d74a5fc8b5bdd1212
Kwang Bin Lee <klee166@jhu.edu>
2016-10-30 22:15:42 -0400

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,42 @@
 class Event < ApplicationRecord
+  attr_accessor :isLit, :name
+  attr_reader :id, :expectedNum, :name, :ticketPrice, :description, :location, :isLit, :numAddedToCal, :numCheckIn
+  
   composed_of :offers, class_name: "Offer", mapping: [%w(name), %w(numUsed), %w(maxUse)]
   composed_of :locations, class_name: "Location", mapping: [%w(building), %w(room), %w(latitude), %w(longitude)]
+
+  def initialize(id, expectedNum, name, ticketPrice, description, location, isLit, numAddedToCal, numCheckIn)
+    @id, @expectedNum, @name, @ticketPrice, @description, @location, @isLit, @numAddedToCal, @numCheckIn = id, expectedNum, name, ticketPrice, description, location, isLit, numAddedToCal, numCheckIn
+  end
+
+  # sets lit value
+  def isLit=(val)
+    self[:isLit] = val
+  end
+
+  # edits the name of the event
+  def name=(val)
+    self[:name] = val
+  end
+
+  #edits the description of the event
+  def description=(val)
+    self[:description] = val
+  end
+
+  #increments the number of checkins 
+  def add_num_checkin
+    self[:numCheckIn] += 1
+  end
+
+  # get the number of added events to calendar
+  def numAddedToCal
+    self[:numAddedToCal]
+  end
+
+  # increments the number of added events to calendar
+  def inc_AddedToCal
+    self[:numAddedToCal] += 1
+  end
+
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -5,7 +5,8 @@ class Offer < ApplicationRecord
     @name, @numUsed, @maxUse = name, numUsed, maxUse
   end
 
-  def increaseUsed
-    numUsed += 1
+  #increment the number of used ones
+  def increase_used
+    self[:numUsed] += 1
   end
 end

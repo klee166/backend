9f6890ccaf6a87f194ad6384f8649bdd99c168a5
Kwang Bin Lee <klee166@jhu.edu>
2016-10-30 21:07:29 -0400

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,4 @@
 class Event < ApplicationRecord
   composed_of :offers, class_name: "Offer", mapping: [%w(name), %w(numUsed), %w(maxUse)]
+  composed_of :locations, class_name: "Location", mapping: [%w(building), %w(room), %w(latitude), %w(longitude)]
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,29 @@
 class Location < ApplicationRecord
+  attr_reader :building, :room, :latitude, :longitude
+  
+
+  def initialize(building, room, latitude, longitude)
+    @building, @room, @latitude, @longitude = building, room, latitude, longitude
+  end
+
+  # gets building
+  def building 
+    @building  
+  end
+
+  # gets room
+  def room
+    @room
+  end
+
+  # gets latitude
+  def latitude
+    @latitude
+  end
+
+  # gets longitude
+  def longitude
+    @longitude
+  end
+
 end
